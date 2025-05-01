9d16f2f7dd0c3652d7dc2404d864284b7a0e2888
oivaniuta <olgaivanyuta8@gmail.com>
2025-05-01 22:52:40 +0300

    Додано приклад до завдання 2

--- jsBasics/task2.js
+++ jsBasics/task2.js
@@ -1,6 +1,7 @@
 let user1 = "John";
 let user2 = "Julia";
 let message = "Ласкаво просимо, " + user1 + " та " + user2 + "! Гарного відпочинку!";
+let message2 = `Ласкаво просимо, ${user1} і ${user2}!`
 
 console.log(message);
-
+console.log(message2);
